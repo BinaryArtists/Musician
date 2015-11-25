ba03ea3bd6767f4b7ba167402f186d1bfd458aec
王涛 <wangtao1@wangtaodeMac-mini.local>
2015-11-25 18:04:20 +0800

    初始化工程，没有任何内容，先写框架性组件

---
+++ SwiftProject/SwiftProject/查找音乐/SearchMusicHomePage.swift
@@ -0,0 +1,19 @@
+//
+//  SearchMusicHomePage.swift
+//  SwiftProject
+//
+//  Created by 王涛 on 15/11/24.
+//  Copyright © 2015年 王涛. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchMusicHomePageVC: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "查找音乐";
+    }
+
+}
