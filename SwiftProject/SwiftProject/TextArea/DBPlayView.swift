f222380873378c88c29fcad45332c5dbaefa747d
王云鹏 <571533442@qq.com>
2016-03-10 09:58:29 +0800

    加入小ui修改

--- SwiftProject/SwiftProject/TextArea/DBPlayView.swift
+++ SwiftProject/SwiftProject/TextArea/DBPlayView.swift
@@ -43,9 +43,20 @@ class DBPlayView: UIView {
         let pan = UIPanGestureRecognizer(target: self, action: "pan:")
         addGestureRecognizer(pan)
         setSubviews()
+        self.addAction { () -> () in
+            guard self.showing == DBFalse else{
+                return
+            }
+            self.showing = DBTrue
+            self.viewHeightConstant = DBHeight - topBarHeight
+            self.imageHeight = DBWidth/2
+            self.imageLeftMargin = DBWidth/4
+            self.configSubviewWhenEnded()
+        }
     }
     
     func setSubviews(){
+        //imageView
         self.addSubview(self.playImageView)
         self.playImageView.snp_makeConstraints { (make) -> Void in
             make.height.width.equalTo(imageHeight)
@@ -53,9 +64,41 @@ class DBPlayView: UIView {
             make.top.equalTo(self).offset(imageTopMargin)
         }
         playImageView.image = UIImage(named: "headerImage")
+        
+        //bottomLabels
+        let serialLabel = factoryLabel()
+        serialLabel.text = "轻音乐 系 MHz"
+        serialLabel.font = leve1004Font
+        serialLabel.textColor = gray004Color
+        insertSubview(serialLabel, belowSubview: playImageView)
+        serialLabel.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(self.snp_bottom).offset(-10)
+            make.left.equalTo(self).offset(60)
+        }
+        
+        let musicNameLabel = factoryLabel()
+        musicNameLabel.text = "Music name"
+        musicNameLabel.font = leve1002Font
+        musicNameLabel.textColor = gray002Color
+        insertSubview(musicNameLabel, belowSubview: playImageView)
+        musicNameLabel.snp_makeConstraints { (make) -> Void in
+            make.bottom.equalTo(serialLabel.snp_top).offset(-6)
+            make.left.equalTo(serialLabel)
+        }
+        
+        //bottomButtons
+        
+    }
+    
+    func factoryLabel() -> UILabel{
+        let label = UILabel()
+        label.textAlignment = .Center
+        label.backgroundColor = UIColor.whiteColor()
+        return label
     }
     
 //MARK:GestureRecognizer
+    //FIXME:下面的比例计算的有些许问题，在4s上存在一点点的瑕疵
     func pan(pan:UIPanGestureRecognizer){
        if(pan.state == .Changed){
             let translation = pan.translationInView(self)
@@ -86,19 +129,25 @@ class DBPlayView: UIView {
                 imageLeftMargin = DBWidth/4
                 showing = DBTrue
             }
-            self.playImageView.snp_updateConstraints { (make) -> Void in
-                make.width.height.equalTo(imageHeight)
-                make.left.equalTo(imageLeftMargin)
-                make.top.equalTo(imageLeftMargin)
-            }
-            self.snp_updateConstraints(closure: { (make) -> Void in
-                make.height.equalTo(viewHeightConstant)
-            })
-            UIView.animateWithDuration(0.7, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-                self.layoutIfNeeded()
-            }, completion:nil);
-        
+                configSubviewWhenEnded()
         }
+    }
+    
+    /**
+     1.在pan结束的时候调用 2.在下方点击playView时候调用
+     */
+    func configSubviewWhenEnded(){
+        self.playImageView.snp_updateConstraints { (make) -> Void in
+            make.width.height.equalTo(imageHeight)
+            make.left.equalTo(imageLeftMargin)
+            make.top.equalTo(imageLeftMargin)
+        }
+        self.snp_updateConstraints(closure: { (make) -> Void in
+            make.height.equalTo(viewHeightConstant)
+        })
+        UIView.animateWithDuration(0.7, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.8, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.layoutIfNeeded()
+            }, completion:nil);
     }
     
     override func updateConstraints() {
