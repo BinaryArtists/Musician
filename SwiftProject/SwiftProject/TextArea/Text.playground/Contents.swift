ff57417dfc0682c0a6fb71af5e15d4eecb738f1b
王云鹏 <571533442@qq.com>
2016-03-11 15:31:09 +0800

    try some questions

--- SwiftProject/SwiftProject/TextArea/Text.playground/Contents.swift
+++ SwiftProject/SwiftProject/TextArea/Text.playground/Contents.swift
@@ -2,3 +2,16 @@
 
 import Cocoa
 
+class Animal {
+    var name:String = "ss"
+}
+
+class Person {
+    var name:String! = "xiaohei"
+    var dog:Animal?
+}
+
+let a1 = Animal()
+let p1 = Person()
+p1.dog = a1
+print(p1.dog?.name)
