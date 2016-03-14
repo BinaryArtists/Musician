d271f7b746d2f7ad59cc81c4e2a6bbc5de22c97b
王云鹏 <571533442@qq.com>
2016-03-14 17:52:33 +0800

    测试些高级函数

--- SwiftProject/SwiftProject/TextArea/Text.playground/Contents.swift
+++ SwiftProject/SwiftProject/TextArea/Text.playground/Contents.swift
@@ -15,3 +15,27 @@ let a1 = Animal()
 let p1 = Person()
 p1.dog = a1
 print(p1.dog?.name)
+
+
+//Swift 高级用法
+
+//if case
+let age = 20
+let sex = "girl"
+if case 18...25 = age where sex == "girl"{
+    print("交往吧")
+}
+
+
+var array = [1,2,3,4,5]
+let result1 = array.map { (number) -> String in
+    return String(number)
+}
+let result2 = array.map {
+    return String($0)
+}
+
+let result3 = result1.map { (string) -> Int in
+    return Int(string)!
+}
+
